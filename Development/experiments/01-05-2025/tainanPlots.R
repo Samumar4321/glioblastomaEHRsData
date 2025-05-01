89ce322f5220c29ea17927907ccef40c4b3ffd09
Samumar4321 <samu2003.marelli@gmail.com>
2025-05-01 17:57:25 +0200

    Experiment on Tainandataset plots

---
+++ Development/experiments/01-05-2025/tainanPlots.R
@@ -0,0 +1,116 @@
+source("~/GitHub/glioblastomaEHRsData/Development/bin/loadTestDataset.R")
+p_load("ggplot2", "table1")
+
+df <- tainan2020dataset
+
+df$chemo_yes1_no0 <-
+  factor(df$chemo_yes1_no0,
+         levels = c(0, 1),
+         labels = c("No", "Yes"))
+df$sex_male0_female1 <-
+  factor(df$sex_male0_female1,
+         levels = c(0, 1),
+         labels = c("Male", "Female"))
+df$surgery_yes1_biopsy0 <-
+  factor(df$surgery_yes1_biopsy0,
+         levels = c(0, 1),
+         labels = c("Biopsy", "Yes"))
+df$survived_yes1_no0 <-
+  factor(df$survived_yes1_no0,
+         levels = c(0, 1),
+         labels = c("Dead", "Alive"))
+df$TMZ_based_chemo_yes1_no0 <-
+  factor(df$TMZ_based_chemo_yes1_no0,
+         levels = c(0, 1),
+         labels = c("No", "Yes"))
+df$tumorProgression_no0_yes1 <-
+  factor(df$tumorProgression_no0_yes1,
+         levels = c(0, 1),
+         labels = c("No", "Yes"))
+df$year_of_diagnosis_05.10.0._10.16.1. <-
+  factor(df$year_of_diagnosis_05.10.0._10.16.1.,
+         levels = c(0, 1),
+         labels = c("2005-2010", "2010-2016"))
+
+label(df$age_years) <- "Age (years)"
+label(df$chemo_yes1_no0) <- "Chemotherapy"
+label(df$OS_months) <- "Overall Survival (months)"
+label(df$PFS_months) <- "Progression Free Survival (months)"
+label(df$radiation_dose_Gy) <- "Radiation dose (Gy)"
+label(df$radiation_volume_mL) <- "Radiation volume (mL)"
+label(df$sex_male0_female1) <- "Sex"
+label(df$surgery_yes1_biopsy0) <- "Surgery"
+label(df$survived_yes1_no0) <- "Patient status"
+label(df$TMZ_based_chemo_yes1_no0) <- "TMZ chemotherapy"
+label(df$tumorProgression_no0_yes1) <- "Tumor progression"
+label(df$year_of_diagnosis_05.10.0._10.16.1.) <- "Year of diagnosis"
+
+#Bar plot per Chemo
+ggplot(data = df, aes(x = chemo_yes1_no0,
+                      fill = chemo_yes1_no0)) +
+  geom_bar() +
+  scale_y_continuous(breaks = scales::breaks_width(width = 10)) +
+  scale_fill_brewer(palette = "Accent", na.value = "red") +
+  theme(legend.position = "none") +
+  xlab("Chemotherapy") +
+  ylab("Count")
+
+#Bar plot per Sex
+ggplot(data = df, aes(x = sex_male0_female1,
+                      fill = sex_male0_female1)) +
+  geom_bar() +
+  scale_y_continuous(breaks = scales::breaks_width(width = 10)) +
+  scale_fill_brewer(palette = "Accent", na.value = "red") +
+  theme(legend.position = "none") +
+  xlab("Sex") +
+  ylab("Count")
+
+#Bar plot per Surgery
+ggplot(data = df, aes(x = surgery_yes1_biopsy0,
+                      fill = surgery_yes1_biopsy0)) +
+  geom_bar() +
+  scale_y_continuous(breaks = scales::breaks_width(width = 10)) +
+  scale_fill_brewer(palette = "Accent", na.value = "red") +
+  theme(legend.position = "none") +
+  xlab("Surgery") +
+  ylab("Count")
+
+#Bar plot per Survived
+ggplot(data = df, aes(x = survived_yes1_no0,
+                      fill = survived_yes1_no0)) +
+  geom_bar() +
+  scale_y_continuous(breaks = scales::breaks_width(width = 10)) +
+  scale_fill_brewer(palette = "Accent", na.value = "red") +
+  theme(legend.position = "none") +
+  xlab("Patient status") +
+  ylab("Count")
+
+#Bar plot per TMZ
+ggplot(data = df, aes(x = TMZ_based_chemo_yes1_no0,
+                      fill = TMZ_based_chemo_yes1_no0)) +
+  geom_bar() +
+  scale_y_continuous(breaks = scales::breaks_width(width = 10)) +
+  scale_fill_brewer(palette = "Accent", na.value = "red") +
+  theme(legend.position = "none") +
+  xlab("TMZ based chemotherapy") +
+  ylab("Count")
+
+#Bar plot per TumorProgress
+ggplot(data = df, aes(x = tumorProgression_no0_yes1,
+                      fill = tumorProgression_no0_yes1)) +
+  geom_bar() +
+  scale_y_continuous(breaks = scales::breaks_width(width = 10)) +
+  scale_fill_brewer(palette = "Accent", na.value = "red") +
+  theme(legend.position = "none") +
+  xlab("Tumor progression") +
+  ylab("Count")
+
+#Bar plot per YearOfDiagnosis
+ggplot(data = df, aes(x = year_of_diagnosis_05.10.0._10.16.1.,
+                      fill = year_of_diagnosis_05.10.0._10.16.1.)) +
+  geom_bar() +
+  scale_y_continuous(breaks = scales::breaks_width(width = 10)) +
+  scale_fill_brewer(palette = "Accent", na.value = "red") +
+  theme(legend.position = "none") +
+  xlab("Year of first diagnosis") +
+  ylab("Count")
