a59581a60f03cb6c7b9865c1c6df73c8777b5f9b
Samumar4321 <samu2003.marelli@gmail.com>
2025-05-06 22:07:52 +0200

    Update loadTestDataset.R
    Commented the View comand so it doesn't show the dataset each time

--- Development/bin/loadTestDataset.R
+++ Development/bin/loadTestDataset.R
@@ -5,6 +5,6 @@ munich2019dataset <- read.csv("../../Development/datasets/Munich2019dataset.csv"
 tainan2020dataset <- read.csv("../../Development/datasets/Tainan2020dataset.csv")
 utrecht2019dataset <- read.csv("../../Development/datasets/Utrecht2019dataset.csv")
 
-View(munich2019dataset)
-View(tainan2020dataset)
-View(utrecht2019dataset)
+#View(munich2019dataset)
+#View(tainan2020dataset)
+#View(utrecht2019dataset)
