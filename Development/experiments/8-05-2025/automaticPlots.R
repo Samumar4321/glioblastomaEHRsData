59dea73dac239c7e9d15bc6440d4b521101db1aa
Samumar4321 <samu2003.marelli@gmail.com>
2025-05-19 20:05:04 +0200

    Update automaticPlots.R

--- Development/experiments/8-05-2025/automaticPlots.R
+++ Development/experiments/8-05-2025/automaticPlots.R
@@ -1,4 +1,4 @@
-p_load("summarytools", "dlookr", "DataExplorer")
+p_load("dlookr", "DataExplorer", "dplyr")
 
 #dlookr
 plot_hist_numeric(df,
@@ -14,9 +14,28 @@ plot_bar_category(df,
 
 plot(univar_category(df), prompt = TRUE)
 
+#Target vs altra
+target <- target_by(df, target = post_surgery_therapy_none0_monotherapy1_RT.TMZ2)
+plot(relate(target, survival_months))
+
+#Categorica vs categorica
+all_cat <- compare_category(df)
+plot(all_cat, prompt = TRUE)
+
+#Numerica vs numerica
+all_num <- compare_numeric(df)
+plot(all_num, prompt = TRUE)
+
 eda_web_report(df)
 
 #dataExplorer
-plot_bar(df$cHsp70_low0_high1)$page_1 +
-  geom_text(aes(label = after_stat(y)), position = position_fill(-5)) +
-  labs(y = "Frequency")
+plot_bar(df$post_surgery_therapy_none0_monotherapy1_RT.TMZ2)
+
+plot_histogram(df$age_years)
+
+plot_boxplot(df, by = "SVZ_status_nocontact0_contact1")
+
+plot_scatterplot(df, by = "SVZ_status_nocontact0_contact1",
+                 ncol = 1,
+                 nrow = 1)$page_1 +
+  scale_y_discrete(name = "Hello")
