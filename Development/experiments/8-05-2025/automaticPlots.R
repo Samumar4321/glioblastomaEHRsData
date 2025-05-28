7b6704657066a95fa6da3d51164401f4a372bb9e
Samumar4321 <samu2003.marelli@gmail.com>
2025-05-28 16:38:46 +0200

    Update automaticPlots.R

--- Development/experiments/8-05-2025/automaticPlots.R
+++ Development/experiments/8-05-2025/automaticPlots.R
@@ -1,12 +1,15 @@
 p_load("dlookr", "DataExplorer", "dplyr")
 
 #dlookr
+
+#Numeriche
 plot_hist_numeric(df,
                   "age_years",
                   "survival_months",
                   title = "Histograms of numerical data in munich2019dataset",
                   each = FALSE)
 
+#Categoriche
 plot_bar_category(df,
                   "SVZ_status_nocontact0_contact1",
                   title = "Bar charts of Sex",
@@ -20,22 +23,32 @@ plot(relate(target, survival_months))
 
 #Categorica vs categorica
 all_cat <- compare_category(df)
-plot(all_cat, prompt = TRUE)
+plot(all_cat)
 
 #Numerica vs numerica
 all_num <- compare_numeric(df)
-plot(all_num, prompt = TRUE)
+plot(all_num)
 
 eda_web_report(df)
 
-#dataExplorer
+#DataExplorer
+
+#Categoriche
 plot_bar(df$post_surgery_therapy_none0_monotherapy1_RT.TMZ2)
 
+#Numeriche
 plot_histogram(df$age_years)
 
+#Numeriche vs categorica target
 plot_boxplot(df, by = "SVZ_status_nocontact0_contact1")
 
-plot_scatterplot(df, by = "SVZ_status_nocontact0_contact1",
-                 ncol = 1,
-                 nrow = 1)$page_1 +
-  scale_y_discrete(name = "Hello")
+#Categoriche vs categorica target
+plot_bar(df, by = "SVZ_status_nocontact0_contact1")
+
+#Numeriche vs numerica target
+plot_scatterplot(df, by = "age_years", ncol = 1, nrow = 1)$page_4 +
+  labs(y = "OS in months",
+       x = "Age in years",
+       title = "Age vS Survival") +
+  scale_y_discrete(limits = factor(seq(0, 34, by = 0.01)), breaks = seq(0, 34, by = 1))
+
