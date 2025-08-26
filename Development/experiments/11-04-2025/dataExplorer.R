633202a21e3f241ac877df4f382704cccfc9145a
Samuele Marelli <81413602+Samumar4321@users.noreply.github.com>
2025-08-26 12:23:21 +0200

    moved development to a new branch
    Moved all the development stuff to a new branch, keeping on the main only the package

--- Development/experiments/11-04-2025/dataExplorer.R
+++
@@ -1,4 +0,0 @@
-library("pacman")
-p_load("DataExplorer")
-
-create_report(munich2019dataset)
--- Development/experiments/12-04-2025/testdlookr.R
+++
@@ -1,3 +0,0 @@
-source("~/GitHub/glioblastomaEHRsData/Development/bin/loadTestDataset.R")
-p_load("dlookr")
-diagnose_web_report(utrecht2019dataset)
