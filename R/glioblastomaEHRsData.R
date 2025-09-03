cccb68f39f2e2e71c8ef3cbf6a5f4d513ca34072
Samuele Marelli <81413602+Samumar4321@users.noreply.github.com>
2025-09-03 10:07:18 +0200

    Updated package description

--- R/glioblastomaEHRsData.R
+++ R/glioblastomaEHRsData.R
@@ -1,7 +1,13 @@
 #' glioblastomaEHRsData: Descriptive analysis on 3 EHRs datasets
 #'
-#' Il pacchetto fornisce dataset e funzioni per generare statistiche descrittive,
-#' tabelle di frequenza, riepiloghi e grafici, facilitando l’analisi esplorativa.
+#' Provides functions to load and analyze three open Electronic Health
+#' Records (EHRs) datasets of patients diagnosed with glioblastoma,
+#' previously released under the Creative Commons Attribution 4.0 International (CC BY 4.0) license.
+#' Users can generate basic descriptive statistics, frequency tables and
+#' save descriptive summary table, as well as create and export univariate or
+#' bivariate plots.
+#' The package is designed to work with the included datasets and to facilitate
+#' quick exploratory data analysis and reporting.
 #'
 #' @keywords internal
 "_PACKAGE"
